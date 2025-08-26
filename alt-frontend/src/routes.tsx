91b2e70ffefa88603e1602d7b59f7ae965bd418d
rlossec <raphaelossec@gmail.com>
2025-08-26 10:51:46 +0200

    Add Not Found page

--- alt-frontend/src/routes.tsx
+++ alt-frontend/src/routes.tsx
@@ -1,8 +1,13 @@
 import { DashboardPage } from "./pages/DashboardPage";
 import { AnalyticsPage } from "./pages/AnalyticsPage";
 import { ToolsPage } from "./pages/ToolsPage";
+import { NotFoundPage } from "./pages/common/NotFoundPage";
 
 export const routes = [
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
   {
     path: "/",
     element: <DashboardPage />,
